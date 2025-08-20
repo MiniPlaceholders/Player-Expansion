70447805486d200a84636f8072581dc91be54a80
Fabian Mäkilä <contact@fabianadrian.fi>
2025-08-20 15:45:48 +0300

    remove leftover imports

--- paper/src/main/java/io/github/miniplaceholders/expansion/player/paper/PaperProvider.java
+++ paper/src/main/java/io/github/miniplaceholders/expansion/player/paper/PaperProvider.java
@@ -8,48 +8,44 @@ import io.github.miniplaceholders.expansion.player.common.resolver.NameResolver;
 import io.github.miniplaceholders.expansion.player.paper.resolver.StatisticResolver;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.tag.Tag;
-import org.bukkit.Material;
 import org.bukkit.Server;
-import org.bukkit.Statistic;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 
-import java.util.Locale;
 import java.util.Optional;
 
 public final class PaperProvider extends PlatformExpansionProvider<Server> {
 
-    public PaperProvider(Object platformInstance) {
-        super((Server) platformInstance);
-    }
+	public PaperProvider(Object platformInstance) {
+		super((Server) platformInstance);
+	}
 
-    @Override
-    public Expansion.Builder provideBuilder() {
-        return Expansion.builder("player")
-                .audiencePlaceholder("name", new NameResolver())
-                .audiencePlaceholder("displayname", new DisplayNameResolver())
-                .audiencePlaceholder(Player.class, "ping", (player, queue, ctx) -> {
-                    return Tag.preProcessParsed(Integer.toString(player.getPing()));
-                })
-                .audiencePlaceholder("locale", new LocaleResolver())
-                .audiencePlaceholder(Player.class, "client", (player, queue, ctx) -> {
-                    final String playerClient = player.getClientBrandName();
-                    return Tag.preProcessParsed(playerClient != null
-                            ? playerClient
-                            : "vanilla");
-                })
-                .audiencePlaceholder(Player.class, "world", (player, queue, ctx) -> {
-                    return Tag.preProcessParsed(player.getWorld().getName());
-                })
-                .audiencePlaceholder(Player.class, "team", (player, queue, ctx) -> {
-                    return Tag.selfClosingInserting(player.teamDisplayName());
-                })
-                .audiencePlaceholder(Player.class, "tab_header", (player, queue, ctx) -> {
-                    return Tag.selfClosingInserting(Optional.ofNullable(player.playerListHeader()).orElse(Component.empty()));
-                })
-                .audiencePlaceholder(Player.class, "tab_footer", (player, queue, ctx) -> {
-                    return Tag.selfClosingInserting(Optional.ofNullable(player.playerListFooter()).orElse(Component.empty()));
-                })
-                .audiencePlaceholder("statistic", new StatisticResolver());
-    }
+	@Override
+	public Expansion.Builder provideBuilder() {
+		return Expansion.builder("player")
+				.audiencePlaceholder("name", new NameResolver())
+				.audiencePlaceholder("displayname", new DisplayNameResolver())
+				.audiencePlaceholder(Player.class, "ping", (player, queue, ctx) -> {
+					return Tag.preProcessParsed(Integer.toString(player.getPing()));
+				})
+				.audiencePlaceholder("locale", new LocaleResolver())
+				.audiencePlaceholder(Player.class, "client", (player, queue, ctx) -> {
+					final String playerClient = player.getClientBrandName();
+					return Tag.preProcessParsed(playerClient != null
+							? playerClient
+							: "vanilla");
+				})
+				.audiencePlaceholder(Player.class, "world", (player, queue, ctx) -> {
+					return Tag.preProcessParsed(player.getWorld().getName());
+				})
+				.audiencePlaceholder(Player.class, "team", (player, queue, ctx) -> {
+					return Tag.selfClosingInserting(player.teamDisplayName());
+				})
+				.audiencePlaceholder(Player.class, "tab_header", (player, queue, ctx) -> {
+					return Tag.selfClosingInserting(Optional.ofNullable(player.playerListHeader()).orElse(Component.empty()));
+				})
+				.audiencePlaceholder(Player.class, "tab_footer", (player, queue, ctx) -> {
+					return Tag.selfClosingInserting(Optional.ofNullable(player.playerListFooter()).orElse(Component.empty()));
+				})
+				.audiencePlaceholder("statistic", new StatisticResolver());
+	}
 }
