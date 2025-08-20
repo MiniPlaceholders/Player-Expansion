4acc5ef515e8049c35ef449ab8afaa08eb10376b
Fabian Mäkilä <contact@fabianadrian.fi>
2025-08-20 15:48:53 +0300

    fix indentation

--- paper/src/main/java/io/github/miniplaceholders/expansion/player/paper/resolver/StatisticResolver.java
+++ paper/src/main/java/io/github/miniplaceholders/expansion/player/paper/resolver/StatisticResolver.java
@@ -14,56 +14,56 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Locale;
 
 public final class StatisticResolver implements AudienceTagResolver<@NotNull Audience> {
-	@Override
-	public @NotNull Tag tag(@NotNull Audience audience, @NotNull ArgumentQueue queue, @NotNull Context ctx) {
-		if (!queue.hasNext()) {
-			return Tag.preProcessParsed("You need to provide a statistic");
-		}
+    @Override
+    public @NotNull Tag tag(@NotNull Audience audience, @NotNull ArgumentQueue queue, @NotNull Context ctx) {
+        if (!queue.hasNext()) {
+            return Tag.preProcessParsed("You need to provide a statistic");
+        }
 
-		Statistic statistic;
-		try {
-			statistic = Statistic.valueOf(queue.pop().value().toUpperCase(Locale.ROOT));
-		} catch (IllegalArgumentException e) {
-			return Tag.preProcessParsed("Unknown statistic");
-		}
+        Statistic statistic;
+        try {
+            statistic = Statistic.valueOf(queue.pop().value().toUpperCase(Locale.ROOT));
+        } catch (IllegalArgumentException e) {
+            return Tag.preProcessParsed("Unknown statistic");
+        }
 
-		final Player player = (Player) audience;
+        final Player player = (Player) audience;
 
-		switch (statistic.getType()) {
-			case UNTYPED -> {
-				return Tag.preProcessParsed(String.valueOf(player.getStatistic(statistic)));
-			}
-			case ITEM, BLOCK -> {
-				if (!queue.hasNext()) {
-					return Tag.preProcessParsed("You need to provide a material");
-				}
+        switch (statistic.getType()) {
+            case UNTYPED -> {
+                return Tag.preProcessParsed(String.valueOf(player.getStatistic(statistic)));
+            }
+            case ITEM, BLOCK -> {
+                if (!queue.hasNext()) {
+                    return Tag.preProcessParsed("You need to provide a material");
+                }
 
-				Material material;
-				try {
-					material = Material.valueOf(queue.pop().value().toUpperCase(Locale.ROOT));
-				} catch (IllegalArgumentException e) {
-					return Tag.preProcessParsed("Unknown material");
-				}
+                Material material;
+                try {
+                    material = Material.valueOf(queue.pop().value().toUpperCase(Locale.ROOT));
+                } catch (IllegalArgumentException e) {
+                    return Tag.preProcessParsed("Unknown material");
+                }
 
-				return Tag.preProcessParsed(String.valueOf(player.getStatistic(statistic, material)));
-			}
-			case ENTITY -> {
-				if (!queue.hasNext()) {
-					return Tag.preProcessParsed("You need to provide an entity");
-				}
+                return Tag.preProcessParsed(String.valueOf(player.getStatistic(statistic, material)));
+            }
+            case ENTITY -> {
+                if (!queue.hasNext()) {
+                    return Tag.preProcessParsed("You need to provide an entity");
+                }
 
-				EntityType entity;
-				try {
-					entity = EntityType.valueOf(queue.pop().value().toUpperCase(Locale.ROOT));
-				} catch (IllegalArgumentException e) {
-					return Tag.preProcessParsed("Unknown entity");
-				}
+                EntityType entity;
+                try {
+                    entity = EntityType.valueOf(queue.pop().value().toUpperCase(Locale.ROOT));
+                } catch (IllegalArgumentException e) {
+                    return Tag.preProcessParsed("Unknown entity");
+                }
 
-				return Tag.preProcessParsed(String.valueOf(player.getStatistic(statistic, entity)));
-			}
-			default -> {
-				return Tag.preProcessParsed("Unknown statistic type");
-			}
-		}
-	}
+                return Tag.preProcessParsed(String.valueOf(player.getStatistic(statistic, entity)));
+            }
+            default -> {
+                return Tag.preProcessParsed("Unknown statistic type");
+            }
+        }
+    }
 }
