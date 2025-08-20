c2c00af30a261dd537da13b3ffc0760aa9996abc
Fabian Mäkilä <contact@fabianadrian.fi>
2025-08-20 15:50:49 +0300

    also fix indentation

--- paper/src/main/java/io/github/miniplaceholders/expansion/player/paper/PaperProvider.java
+++ paper/src/main/java/io/github/miniplaceholders/expansion/player/paper/PaperProvider.java
@@ -15,37 +15,37 @@ import java.util.Optional;
 
 public final class PaperProvider extends PlatformExpansionProvider<Server> {
 
-	public PaperProvider(Object platformInstance) {
-		super((Server) platformInstance);
-	}
+    public PaperProvider(Object platformInstance) {
+        super((Server) platformInstance);
+    }
 
-	@Override
-	public Expansion.Builder provideBuilder() {
-		return Expansion.builder("player")
-				.audiencePlaceholder("name", new NameResolver())
-				.audiencePlaceholder("displayname", new DisplayNameResolver())
-				.audiencePlaceholder(Player.class, "ping", (player, queue, ctx) -> {
-					return Tag.preProcessParsed(Integer.toString(player.getPing()));
-				})
-				.audiencePlaceholder("locale", new LocaleResolver())
-				.audiencePlaceholder(Player.class, "client", (player, queue, ctx) -> {
-					final String playerClient = player.getClientBrandName();
-					return Tag.preProcessParsed(playerClient != null
-							? playerClient
-							: "vanilla");
-				})
-				.audiencePlaceholder(Player.class, "world", (player, queue, ctx) -> {
-					return Tag.preProcessParsed(player.getWorld().getName());
-				})
-				.audiencePlaceholder(Player.class, "team", (player, queue, ctx) -> {
-					return Tag.selfClosingInserting(player.teamDisplayName());
-				})
-				.audiencePlaceholder(Player.class, "tab_header", (player, queue, ctx) -> {
-					return Tag.selfClosingInserting(Optional.ofNullable(player.playerListHeader()).orElse(Component.empty()));
-				})
-				.audiencePlaceholder(Player.class, "tab_footer", (player, queue, ctx) -> {
-					return Tag.selfClosingInserting(Optional.ofNullable(player.playerListFooter()).orElse(Component.empty()));
-				})
-				.audiencePlaceholder("statistic", new StatisticResolver());
-	}
+    @Override
+    public Expansion.Builder provideBuilder() {
+        return Expansion.builder("player")
+                .audiencePlaceholder("name", new NameResolver())
+                .audiencePlaceholder("displayname", new DisplayNameResolver())
+                .audiencePlaceholder(Player.class, "ping", (player, queue, ctx) -> {
+                    return Tag.preProcessParsed(Integer.toString(player.getPing()));
+                })
+                .audiencePlaceholder("locale", new LocaleResolver())
+                .audiencePlaceholder(Player.class, "client", (player, queue, ctx) -> {
+                    final String playerClient = player.getClientBrandName();
+                    return Tag.preProcessParsed(playerClient != null
+                            ? playerClient
+                            : "vanilla");
+                })
+                .audiencePlaceholder(Player.class, "world", (player, queue, ctx) -> {
+                    return Tag.preProcessParsed(player.getWorld().getName());
+                })
+                .audiencePlaceholder(Player.class, "team", (player, queue, ctx) -> {
+                    return Tag.selfClosingInserting(player.teamDisplayName());
+                })
+                .audiencePlaceholder(Player.class, "tab_header", (player, queue, ctx) -> {
+                    return Tag.selfClosingInserting(Optional.ofNullable(player.playerListHeader()).orElse(Component.empty()));
+                })
+                .audiencePlaceholder(Player.class, "tab_footer", (player, queue, ctx) -> {
+                    return Tag.selfClosingInserting(Optional.ofNullable(player.playerListFooter()).orElse(Component.empty()));
+                })
+                .audiencePlaceholder("statistic", new StatisticResolver());
+    }
 }
